2cea36c41b7f84a5b0e0baed124970ca8266207e
Vic0716 <vic3962449@gmail.com>
2021-07-29 20:30:04 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/menudemo/MainActivity2.java
@@ -0,0 +1,22 @@
+package tw.com.menudemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button endbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        endbtn = findViewById(R.id.endbtn);
+        endbtn.setOnClickListener(v -> {
+            finish(); //關閉此頁
+        });
+    }
+}
